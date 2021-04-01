92f4f26639eedb00c4b12023447a75ada39ca981
Clément Hamada <clementhamada@protonmail.com>
2021-04-01 17:32:53 +0200

    IRCServer: Implement MotdCommand

--- include/IRCReplies.hpp
+++ include/IRCReplies.hpp
@@ -15,6 +15,43 @@ namespace irc
 {
 	class	Client;
 
+	template<typename Cstart, typename Ccontent, typename Cend>
+	std::string	serializeReplyList(std::string const& serverName,
+		std::string const& nickName, std::string const& list, char delim = '\n',
+		size_t maxLength = 80)
+	{
+		(void)static_cast<IReply*>((Cstart*)NULL);
+		(void)static_cast<IReply*>((Ccontent*)NULL);
+		(void)static_cast<IReply*>((Cend*)NULL);
+
+		std::string	message;
+
+		std::string::size_type	pos = 0;
+		std::string::size_type	newPos = list.find(delim, pos);
+
+		if (newPos == std::string::npos)
+			newPos = list.length();
+		if (newPos > pos + maxLength)
+			newPos = maxLength;
+
+		message.append(Cstart(serverName, nickName, list.substr(pos, newPos)).serialize());
+		while (newPos < list.length())
+		{
+			pos = newPos + 1;
+			newPos = list.find(delim, pos);
+
+			if (newPos == std::string::npos)
+				newPos = list.length();
+			if (newPos > pos + maxLength)
+				newPos = pos + maxLength;
+
+			message.append(Ccontent(serverName, nickName, list.substr(pos, newPos)).serialize());
+		}
+
+		message.append(Cend(serverName, nickName).serialize());
+		return (message);
+	}
+
 	struct	NumericReply	:	IReply
 	{
 		typedef	std::vector<Client const*> clientList;
@@ -39,4 +76,24 @@ namespace irc
 
 	struct UserhostReply	:	NumericReply
 	{ UserhostReply(std::string const& serverName, clientList users); };
+
+	struct MotdStartReply	:	NumericReply
+	{
+		MotdStartReply(std::string const& serverName,
+			std::string const& nickName, std::string const& motd)
+			throw(InvalidMessageException);
+	};
+
+	struct MotdReply		:	NumericReply
+	{
+		MotdReply(std::string const& serverName, std::string const& nickName,
+			std::string const& motd)
+			throw(InvalidMessageException);
+	};
+
+	struct EndOfMotdReply	:	NumericReply
+	{
+		EndOfMotdReply(std::string const& serverName,
+			std::string const& nickName);
+	};
 }
