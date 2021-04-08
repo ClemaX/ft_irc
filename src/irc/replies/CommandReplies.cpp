8cd94fa8d05241868a2f1e8f645b64e57d361278
bjacob-56 <bricejacob56@gmail.com>
2021-04-08 08:38:51 +0200

    update replies files

--- src/irc/replies/CommandReplies.cpp
+++ src/irc/replies/CommandReplies.cpp
@@ -3,6 +3,21 @@
 
 namespace irc
 {
+
+
+// 001     IRC_RPL_WELCOME
+//             "Welcome to the Internet Relay Network <nick>!<user>@<host>"
+// 002     IRC_RPL_YOURHOST
+//             "Your host is <servername>, running version <ver>"
+// 003     IRC_RPL_CREATED
+//             "This server was created <date>"
+// 004     IRC_RPL_MYINFO
+//             "<servername> <version> <available user modes> <available channel modes>"
+// 005     IRC_RPL_BOUNCE
+//             "Try server <server name>, port <port number>"
+
+// 302     IRC_RPL_USERHOST
+//             ":[<reply>{<space><reply>}]"
 	UserhostReply::UserhostReply(std::string const& serverName, clientList users)
 		: NumericReply(serverName, IRC_RPL_USERHOST)
 	{
@@ -12,7 +27,87 @@ namespace irc
 		}
 	}
 
-	MotdStartReply::MotdStartReply(std::string const& serverName, std::string const& nickName, std::string const& motd)
+// 303     IRC_RPL_ISON
+//             ":[<nick> {<space><nick>}]"
+
+// 301     IRC_RPL_AWAY
+//             "<nick> :<away message>"
+
+// 305     IRC_RPL_UNAWAY
+//             ":You are no longer marked as being away"
+// 306     IRC_RPL_NOWAWAY
+//             ":You have been marked as being away"
+
+// 311     IRC_RPL_WHOISUSER
+//             "<nick> <user> <host> * :<real name>"
+// 312     IRC_RPL_WHOISSERVER
+//             "<nick> <server> :<server info>"
+// 313     IRC_RPL_WHOISOPERATOR
+//             "<nick> :is an IRC operator"
+// 317     IRC_RPL_WHOISIDLE
+//             "<nick> <integer> :seconds idle"
+// 318     IRC_RPL_ENDOFWHOIS
+//             "<nick> :End of /WHOIS list"
+// 319     IRC_RPL_WHOISCHANNELS
+//             "<nick> :{[@|+]<channel><space>}"
+
+// 314     IRC_RPL_WHOWASUSER
+//             "<nick> <user> <host> * :<real name>"
+// 369     IRC_RPL_ENDOFWHOWAS
+//             "<nick> :End of WHOWAS"
+
+// 321     IRC_RPL_LISTSTART
+//             "Channel :Users Name"
+// 322     IRC_RPL_LIST
+//             "<channel> <# visible> :<topic>"
+// 323     IRC_RPL_LISTEND
+//             ":End of /LIST"
+
+// 324     IRC_RPL_CHANNELMODEIS
+//             "<channel> <mode> <mode params>"
+
+// 331     IRC_RPL_NOTOPIC
+//             "<channel> :No topic is set"
+// 332     IRC_RPL_TOPIC
+//             "<channel> :<topic>"
+
+// 341     IRC_RPL_INVITING
+//             "<channel> <nick>"
+
+// 342     IRC_RPL_SUMMONING
+//             "<user> :Summoning user to IRC"
+
+// 351     IRC_RPL_VERSION
+//             "<version>.<debuglevel> <server> :<comments>"
+
+// 352     IRC_RPL_WHOREPLY
+//             "<channel> <user> <host> <server> <nick> <H|G>[*][@|+] :<hopcount> <real name>"
+// 315     IRC_RPL_ENDOFWHO
+//             "<name> :End of /WHO list"
+
+// 353     IRC_RPL_NAMREPLY
+//             "<channel> :[[@|+]<nick> [[@|+]<nick> [...]]]"
+// 366     IRC_RPL_ENDOFNAMES
+//             "<channel> :End of /NAMES list"
+
+// 364     IRC_RPL_LINKS
+//             "<mask> <server> :<hopcount> <server info>"
+// 365     IRC_RPL_ENDOFLINKS
+//             "<mask> :End of /LINKS list"
+
+// 367     IRC_RPL_BANLIST
+//             "<channel> <banid>"
+// 368     IRC_RPL_ENDOFBANLIST
+//             "<channel> :End of channel ban list"
+
+// 371     IRC_RPL_INFO
+//             ":<string>"
+// 374     IRC_RPL_ENDOFINFO
+//             ":End of /INFO list"
+
+// 375     IRC_RPL_MOTDSTART
+//             ":- <server> Message of the day - "
+MotdStartReply::MotdStartReply(std::string const& serverName, std::string const& nickName, std::string const& motd)
 		throw(InvalidMessageException)
 		: NumericReply(serverName, IRC_RPL_MOTDSTART, nickName)
 	{
@@ -25,6 +120,8 @@ namespace irc
 			.append(" -");
 	}
 
+// 372     IRC_RPL_MOTD
+//             ":- <text>"
 	MotdReply::MotdReply(std::string const& serverName,
 		std::string const& nickName, std::string const& motd)
 		throw(InvalidMessageException)
@@ -37,6 +134,8 @@ namespace irc
 		message.append(":- ").append(motd).append(" -");
 	}
 
+// 376     IRC_RPL_ENDOFMOTD
+//             ":End of /MOTD command"
 	EndOfMotdReply::EndOfMotdReply(std::string const& serverName,
 		std::string const& nickName)
 		: NumericReply(serverName, IRC_RPL_ENDOFMOTD, nickName)
@@ -46,5 +145,89 @@ namespace irc
 		message.append(":End of /MOTD command.");
 	}
 
-	
+// 381     IRC_RPL_YOUREOPER
+//             ":You are now an IRC operator"
+
+// 382     IRC_RPL_REHASHING
+//             "<config file> :Rehashing"
+
+// 391     IRC_RPL_TIME
+//             "<server> :<string showing server's local time>"
+
+// 392     IRC_RPL_USERSSTART
+//             ":UserID Terminal Host"
+// 393     IRC_RPL_USERS
+//             ":%-8s %-9s %-8s"
+// 394     IRC_RPL_ENDOFUSERS
+//             ":End of users"
+// 395     IRC_RPL_NOUSERS
+//             ":Nobody logged in"
+
+// 200     IRC_RPL_TRACELINK
+//             "Link <version & debug level> <destination> <next server>"
+// 201     IRC_RPL_TRACECONNECTING
+//             "Try. <class> <server>"
+// 202     IRC_RPL_TRACEHANDSHAKE
+//             "H.S. <class> <server>"
+// 203     IRC_RPL_TRACEUNKNOWN
+//             "???? <class> [<client IP address in dot form>]"
+// 204     IRC_RPL_TRACEOPERATOR
+//             "Oper <class> <nick>"
+// 205     IRC_RPL_TRACEUSER
+//             "User <class> <nick>"
+// 206     IRC_RPL_TRACESERVER
+//             "Serv <class> <int>S <int>C <server> <nick!user|!>@<host|server>"
+// 208     IRC_RPL_TRACENEWTYPE
+//             "<newtype> 0 <client name>"
+// 261     IRC_RPL_TRACELOG
+//             "File <logfile> <debug level>"
+
+// 211     IRC_RPL_STATSLINKINFO
+//             "<linkname> <sendq> <sent messages> <sent bytes> <received messages> <received bytes> <time open>"
+// 212     IRC_RPL_STATSCOMMANDS
+//             "<command> <count>"
+// 213     IRC_RPL_STATSCLINE
+//             "C <host> * <name> <port> <class>"
+// 214     IRC_RPL_STATSNLINE
+//             "N <host> * <name> <port> <class>"
+// 215     IRC_RPL_STATSILINE
+//             "I <host> * <host> <port> <class>"
+// 216     IRC_RPL_STATSKLINE
+//             "K <host> * <username> <port> <class>"
+// 218     IRC_RPL_STATSYLINE
+//             "Y <class> <ping frequency> <connect frequency> <max sendq>"
+// 219     IRC_RPL_ENDOFSTATS
+//             "<stats letter> :End of /STATS report"
+// 241     IRC_RPL_STATSLLINE
+//             "L <hostmask> * <servername> <maxdepth>"
+// 242     IRC_RPL_STATSUPTIME
+//             ":Server Up %d days %d:%02d:%02d"
+// 243     IRC_RPL_STATSOLINE
+//             "O <hostmask> * <name>"
+// 244     IRC_RPL_STATSHLINE
+//             "H <hostmask> * <servername>"
+
+// 221     IRC_RPL_UMODEIS
+//             "<user mode string>"
+
+// 251     IRC_RPL_LUSERCLIENT
+//             ":There are <integer> users and <integer> invisible on <integer> servers"
+// 252     IRC_RPL_LUSEROP
+//             "<integer> :operator(s) online"
+// 253     IRC_RPL_LUSERUNKNOWN
+//             "<integer> :unknown connection(s)"
+// 254     IRC_RPL_LUSERCHANNELS
+//             "<integer> :channels formed"
+// 255     IRC_RPL_LUSERME
+//             ":I have <integer> clients and <integer> servers"
+
+// 256     IRC_RPL_ADMINME
+//             "<server> :Administrative info"
+// 257     IRC_RPL_ADMINLOC1
+//             ":<admin info>"
+// 258     IRC_RPL_ADMINLOC2
+//             ":<admin info>"
+// 259     IRC_RPL_ADMINEMAIL
+//             ":<admin info>"
+
 }
--- src/irc/replies/ErrorReplies.cpp
+++ src/irc/replies/ErrorReplies.cpp
@@ -3,6 +3,92 @@
 
 namespace irc
 {
+	
+
+
+// 401     IRC_ERR_NOSUCHNICK
+//             "<nickname> :No such nick/channel"
+
+// 402     IRC_ERR_NOSUCHSERVER
+//             "<server name> :No such server"
+
+// 403     IRC_ERR_NOSUCHCHANNEL
+//             "<channel name> :No such channel"
+
+// 404     IRC_ERR_CANNOTSENDTOCHAN
+//             "<channel name> :Cannot send to channel"
+
+// 405     IRC_ERR_TOOMANYCHANNELS
+//             "<channel name> :You have joined too many channels"
+
+// 406     IRC_ERR_WASNOSUCHNICK
+//             "<nickname> :There was no such nickname"
+
+// 407     IRC_ERR_TOOMANYTARGETS
+//             "<target> :Duplicate recipients. No message"
+
+// 409     IRC_ERR_NOORIGIN
+//             ":No origin specified"
+
+// 411     IRC_ERR_NORECIPIENT
+//             ":No recipient given (<command>)"
+
+// 412     IRC_ERR_NOTEXTTOSEND
+//             ":No text to send"
+
+// 413     IRC_ERR_NOTOPLEVEL
+//             "<mask> :No toplevel domain specified"
+
+// 414     IRC_ERR_WILDTOPLEVEL
+//             "<mask> :Wildcard in toplevel domain"
+
+// 421     IRC_ERR_UNKNOWNCOMMAND
+//             "<command> :Unknown command"
+
+// 422     IRC_ERR_NOMOTD
+//             ":MOTD File is missing"
+
+// 423     IRC_ERR_NOADMININFO
+//             "<server> :No administrative info available"
+
+// 424     IRC_ERR_FILEERROR
+//             ":File error doing <file op> on <file>"
+
+// 431     IRC_ERR_NONICKNAMEGIVEN
+//             ":No nickname given"
+
+// 432     IRC_ERR_ERRONEUSNICKNAME
+//             "<nick> :Erroneus nickname"
+
+// 433     IRC_ERR_NICKNAMEINUSE
+//             "<nick> :Nickname is already in use"
+
+// 436     IRC_ERR_NICKCOLLISION
+//             "<nick> :Nickname collision KILL"
+
+// 441     IRC_ERR_USERNOTINCHANNEL
+//             "<nick> <channel> :They aren't on that channel"
+
+// 442     IRC_ERR_NOTONCHANNEL
+//             "<channel> :You're not on that channel"
+
+// 443     IRC_ERR_USERONCHANNEL
+//             "<user> <channel> :is already on channel"
+
+// 444     IRC_ERR_NOLOGIN
+//             "<user> :User not logged in"
+
+// 445     IRC_ERR_SUMMONDISABLED
+//             ":SUMMON has been disabled"
+
+// 446     IRC_ERR_USERSDISABLED
+//             ":USERS has been disabled"
+
+// 451     IRC_ERR_NOTREGISTERED
+//             ":You have not registered"
+
+// 461     IRC_ERR_NEEDMOREPARAMS
+//             "<command> :Not enough parameters"
 	NeedMoreParamsReply::NeedMoreParamsReply(std::string const& serverName,
 		std::string const& commandName)
 		:	NumericReply(serverName, IRC_ERR_NEEDMOREPARAMS)
@@ -10,5 +96,52 @@ namespace irc
 		message.append(commandName).append(": Not enough parameters");
 	}
 
-	
+// 462     IRC_ERR_ALREADYREGISTRED
+//             ":You may not reregister"
+
+// 463     IRC_ERR_NOPERMFORHOST
+//             ":Your host isn't among the privileged"
+
+// 464     IRC_ERR_PASSWDMISMATCH
+//             ":Password incorrect"
+
+// 465     IRC_ERR_YOUREBANNEDCREEP
+//             ":You are banned from this server"
+
+// 467     IRC_ERR_KEYSET
+//             "<channel> :Channel key already set"
+
+// 471     IRC_ERR_CHANNELISFULL
+//             "<channel> :Cannot join channel (+l)"
+
+// 472     IRC_ERR_UNKNOWNMODE
+//             "<char> :is unknown mode char to me"
+
+// 473     IRC_ERR_INVITEONLYCHAN
+//             "<channel> :Cannot join channel (+i)"
+
+// 474     IRC_ERR_BANNEDFROMCHAN
+//             "<channel> :Cannot join channel (+b)"
+
+// 475     IRC_ERR_BADCHANNELKEY
+//             "<channel> :Cannot join channel (+k)"
+
+// 481     IRC_ERR_NOPRIVILEGES
+//             ":Permission Denied- You're not an IRC operator"
+
+// 482     IRC_ERR_CHANOPRIVSNEEDED
+//             "<channel> :You're not channel operator"
+
+// 483     IRC_ERR_CANTKILLSERVER
+//             ":You cant kill a server!"
+
+// 491     IRC_ERR_NOOPERHOST
+//             ":No O-lines for your host"
+
+// 501     IRC_ERR_UMODEUNKNOWNFLAG
+//             ":Unknown MODE flag"
+
+// 502     IRC_ERR_USERSDONTMATCH
+//             ":Cant change mode for other users"
+
 }
