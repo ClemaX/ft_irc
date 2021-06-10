0d4cf7e3de8cf51b0229d716f31120573f711e18
Clement Hamada <clementhamada@protonmail.com>
2021-06-10 13:20:24 +0200

    WhoReply: Fix parameter spacing

--- src/irc/replies/command/351-Who.cpp
+++ src/irc/replies/command/351-Who.cpp
@@ -8,11 +8,15 @@ namespace NAMESPACE_IRC
 	WhoReply::WhoReply(std::string const& serverName, std::string const &mask, AClient *client, int op)	// need to add host, server, hopcount
 		: NumericReply(serverName, IRC_RPL_WHOREPLY)
 	{
-		message << mask << " " << serverName << " " << client->nickname;
-		if (op == 1)
-			message << " @";
-		else if (!op)
-			message << " +";
-		message << " " << client->username;
+		if (!mask.empty())
+			message += mask + " ";
+
+		message += serverName + " " + client->nickname;
+		if (op == 0)
+			message += " +";
+		else if (op == 1)
+			message += " @";
+
+		message += " " + client->username;
 	}
 }
